d2b82d65ef14df4babf5a768732adf4a9e5c7720
androowl <multiad05@gmail.com>
2020-08-22 22:31:20 +0900

    Customer.js created

---
+++ src/Components/Customer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import App from '../App';
+
+class Customer extends React.Component {
+    render() {
+        return() {
+            <div>
+            <h2>강민신</h2>
+            <p>650128</p>
+            <p>남자</p>
+            <p>사업가</p>
+        </div>
+        }
+    }
+}
+
+export default App;
