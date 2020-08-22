15b96afe58282f0df84cd6bdddeff00407301422
androowl <multiad05@gmail.com>
2020-08-23 00:00:19 +0900

    CustomerProfile,  CustomerInfo created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import Customer from './Components/Customer'
 import './App.css';
 
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/128/128/any',
+  'name': '강민신',
+  'birthday': '650128',
+  'gender': '여자',
+  'job': '사업가'
+}
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-            Learn React
-      </div>
+      <Customer
+        id={customer.id}
+        image={customer.image}
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
     );
 }
 }
--- src/Components/Customer.js
+++ src/Components/Customer.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import App from '../App';
-
 class Customer extends React.Component {
     render() {
-        return() {
+        return (
             <div>
-            <h2>강민신</h2>
-            <p>650128</p>
-            <p>남자</p>
-            <p>사업가</p>
-        </div>
-        }
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+            </div>
+        )
     }
 }
 
-export default App;
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <image src={this.props.image} alt="profile" />   
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
+export default Customer;
