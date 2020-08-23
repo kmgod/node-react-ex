7deff5b1fd2059e840450d5ec1bd01ce5e6ba59a
androowl <multiad05@gmail.com>
2020-08-23 14:42:56 +0900

    async await 적용 package.json 프록시 파일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -19,31 +19,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '강민신',
-  'birthday': '650128',
-  'gender': '남자',
-  'job': '사업가'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '이세인',
-  'birthday': '720223',
-  'gender': '여자',
-  'job': '회사원'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '강체로',
-  'birthday': '120504',
-  'gender': '아이',
-  'job': '초딩생'
-}]
+
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+  
+  componentDisMount() {
+    this.callApi()
+    .then(res => this.SVGElementInstanceList({customers: res}))
+    .catch(err => console.log(err));
+  }
+  
+  CallApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
     const { classes } = this.props;
     return (
@@ -61,7 +54,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            { customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);}) }
+            { this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})
+              : "" }
         </TableBody>
         </Table>
       </Paper>
