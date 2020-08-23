77f59f1a14e45d8761dfa0d9003cfef84470ccef
androowl <multiad05@gmail.com>
2020-08-23 14:14:31 +0900

    express server setting

--- server.js
+++ server.js
@@ -6,9 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/customers', (req, res) => {
 
-    res.send({message: 'Hello Express!'});
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': '강민신',
+        'birthday': '650128',
+        'gender': '남자',
+        'job': '사업가'
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': '이세인',
+        'birthday': '720223',
+        'gender': '여자',
+        'job': '회사원'
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': '강체로',
+        'birthday': '120504',
+        'gender': '아이',
+        'job': '초딩생'
+      }]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
